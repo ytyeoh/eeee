0b78179804f921b2f70ca4163f06aa113e44a60d
ytyeoh <ytyeoh@users.noreply.github.com>
2022-07-14 10:58:17 +0800

    ad panel

--- custom.js
+++ custom.js
@@ -3,14 +3,17 @@ var html ='';
 jQuery.each( array, function( i, val ) {
 
   ii = i+1;
-  html += '<div class="card"><div class="card-header" role="tab" id="a'+ii+'"><a data-toggle="collapse" href="#'+ii+'" class="" aria-expanded="true" aria-controls="collapseOne"><h5 class="mb-0">'+ii+'</h5></a></div><div id="'+ii+'" class="collapse" role="tabpanel" aria-labelledby="headingOne" data-parent="#accordion"><div class="card-body">'+val+'</div></div></div>';
+  html += '<div class="panel panel-default"><div class="panel-heading"><a data-toggle="collapse" data-parent="#accordion" href="#'+ii+'"><h4 class="panel-title">'+ii+'</h4></a></div><div id="'+ii+'" class="panel-collapse collapse in"><div class="panel-body">'+val+'</div></div></div>';
   // Will stop running after "three"
   
 });
 // debugger;
 $('#accordion').append(html);
 
-$( "h5" ).click(function() {
-    $( this ).parent().addClass('btn btn-info  btn-lg btn-block ');
-    // $( this ).parent().removeClass('btn-success');
+$( "h4" ).click(function() {
+  
+    $( this ).parent().parent().parent().addClass('panel-success');
+    $( this ).parent().parent().parent().removeClass('panel-default');
 });
+
+
